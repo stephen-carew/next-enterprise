b8062e281b72dcea6126ef21af45737199f5fa3a
Stephen Carew <stephencarew1@gmail.com>
2025-05-19 23:49:10 +0100

    create deployment

--- app/order-status/[orderId]/page.tsx
+++ app/order-status/[orderId]/page.tsx
@@ -3,13 +3,7 @@ import * as RadixDialog from "@radix-ui/react-dialog";
 import { useEffect, useState } from "react";
 import { Button } from "../../../components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "../../../components/ui/card";
-
-
-
-
 import { Input } from "../../../components/ui/input";
-
-
 import { Drink, Order } from "../../../lib/types";
 
 
