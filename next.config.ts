4d0b928bc08ab639b86cd5aa061ab1410a50f386
Stephen Carew <stephencarew1@gmail.com>
2025-05-20 08:46:04 +0100

    get rid of env

--- next.config.ts
+++ next.config.ts
@@ -10,12 +10,7 @@ const config: NextConfig = {
       fullUrl: true,
     },
   },
-  env: {
-    NEXT_PUBLIC_PUSHER_APP_ID: process.env.NEXT_PUBLIC_PUSHER_APP_ID,
-    NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
-    NEXT_PUBLIC_PUSHER_SECRET: process.env.NEXT_PUBLIC_PUSHER_SECRET,
-    NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-  },
+
   rewrites: async () => [
     { source: "/healthz", destination: "/api/health" },
     { source: "/api/healthz", destination: "/api/health" },
