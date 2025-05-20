af50c26f7876adaf83b7e0b7c513282c6fbc54e6
Stephen Carew <stephencarew1@gmail.com>
2025-05-20 08:47:25 +0100

    ji

--- env.mjs
+++ env.mjs
@@ -7,21 +7,9 @@ export const env = createEnv({
       .enum(["true", "false"])
       .optional()
       .transform((value) => value === "true"),
-    NEXT_PUBLIC_PUSHER_APP_ID: z.string(),
-    NEXT_PUBLIC_PUSHER_KEY: z.string(),
-    NEXT_PUBLIC_PUSHER_SECRET: z.string(),
-    NEXT_PUBLIC_PUSHER_CLUSTER: z.string(),
   },
-  client: {
-    NEXT_PUBLIC_PUSHER_APP_ID: z.string(),
-    NEXT_PUBLIC_PUSHER_KEY: z.string(),
-    NEXT_PUBLIC_PUSHER_CLUSTER: z.string(),
-  },
+  client: {},
   runtimeEnv: {
     ANALYZE: process.env.ANALYZE,
-    NEXT_PUBLIC_PUSHER_APP_ID: process.env.NEXT_PUBLIC_PUSHER_APP_ID,
-    NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
-    NEXT_PUBLIC_PUSHER_SECRET: process.env.NEXT_PUBLIC_PUSHER_SECRET,
-    NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
   },
 })
